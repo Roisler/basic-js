a41997d9282afdd1c5134f0a25284d15a3ef2076
Roisler <roislerxs@gmail.com>
2023-12-17 19:18:06 +0700

    feat: implemented transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  
+  const newArr = [...arr];
+  const obj = Object.assign({}, newArr);
+
+  for (let i = 0; i < newArr.length; i += 1) {
+    switch (obj[i]) {
+      case '--discard-next':
+        obj[i] = [];
+        if (i === newArr.length - 1) {
+          break;
+        }
+        obj[i + 1] = [];
+        break;
+      case '--discard-prev':
+        obj[i] = [];
+        if (i === 0) {
+          break;
+        }
+        obj[i - 1] = [];
+        break;
+      case '--double-next':
+        if (i === newArr.length - 1) {
+          obj[i] = [];
+          break;
+        }
+        obj[i] = obj[i + 1];
+        break;
+      case '--double-prev':
+        if (i === 0) {
+          obj[i] = [];
+          break;
+        }
+        obj[i] = obj[i - 1];
+        break;
+      default:
+        break;
+    }
+  };
+
+  const result = [];
+  const keys = Object.keys(obj);
+
+  keys.forEach((key) => {
+    result.push(obj[key]);
+  });
+  return result.flat();
+};
 
 module.exports = {
   transform
