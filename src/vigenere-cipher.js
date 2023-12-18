d4e28ab14ac661dca9270051b50f7bd30ba939ca
Roisler <roislerxs@gmail.com>
2023-12-19 01:32:58 +0700

    feat: implemented vigener

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.machineType = type;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    };
+
+    const messageUpperCase = message.toUpperCase();
+    const keyUpperCase = key.toUpperCase();
+
+    const lengthMessage = messageUpperCase.length;
+    const keyLengthSize = keyUpperCase.padEnd(lengthMessage, keyUpperCase);
+    const arrOfIndexKey = keyLengthSize
+        .split('')
+        .map((el) => Number(this.alphabet.indexOf(el) + 1));
+
+    const arrOfMessage = messageUpperCase
+        .split('');
+    
+    const encryptedMessage = [];
+
+    let i = 0;
+    arrOfMessage.forEach((el) => {
+      const indexEl = this.alphabet.indexOf(el);
+      if (indexEl === -1) {
+        encryptedMessage.push(el);
+      } else {
+        const index = (indexEl + arrOfIndexKey[i] - 1) % this.alphabet.length;
+        encryptedMessage.push(this.alphabet[index]);
+        i += 1;
+      }
+    })
+
+    return this.machineType ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
   }
-}
+  decrypt(encryptMessage, key) {
+    if (!encryptMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const encryptMessageUpperCase = encryptMessage.toUpperCase();
+    const keyUpperCase = key.toUpperCase();
+
+    const lengthMessage = encryptMessageUpperCase.length;
+    const keyLengthSize = keyUpperCase.padEnd(lengthMessage, keyUpperCase);
+    const arrOfIndexKey = keyLengthSize
+        .split('')
+        .map((el) => Number(this.alphabet.indexOf(el) + 1));
+
+    const arrOfMessage = encryptMessageUpperCase
+        .split('');
+    
+    const decryptedMessage = [];
+
+    let i = 0;
+    arrOfMessage.forEach((el) => {
+      const indexEl = this.alphabet.indexOf(el);
+      if (indexEl === -1) {
+        decryptedMessage.push(el);
+      } else {
+        const index = (indexEl + this.alphabet.length - arrOfIndexKey[i] + 1) % this.alphabet.length;
+        decryptedMessage.push(this.alphabet[index]);
+        i += 1;
+      }
+    })
+
+    return this.machineType ? decryptedMessage.join('') : decryptedMessage.reverse().join('');
+  }
+};
 
 module.exports = {
   VigenereCipheringMachine
